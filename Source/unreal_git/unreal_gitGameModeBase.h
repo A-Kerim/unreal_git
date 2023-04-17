cf9c0459aedb848c36fc12f141d0c62f6c7ec6f6
A-Kerim <a.kerim@lancaster.ac.uk>
2023-04-17 12:49:09 +0100

    init
    This is the fist version of my Unreal project. I have only one material!

---
+++ Source/unreal_git/unreal_gitGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "unreal_gitGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_GIT_API Aunreal_gitGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
